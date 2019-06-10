f08e86992bae460bd1465b25b940f63587bb5747
jenapidev <jeanpierrenieto@gmail.com>
2019-06-10 01:57:42 -0400

    fixing small bug

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,7 +43,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
-    'users.apps.UsersAppConfig',
+    'cride.users.apps.UsersAppConfig',
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
