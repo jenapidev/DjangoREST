0e497e401819cb9d69db282aadbf75c5bc069af8
jenapidev <jeanpierrenieto@gmail.com>
2019-06-15 12:17:43 -0400

    Added REST Framework

--- config/settings/base.py
+++ config/settings/base.py
@@ -157,3 +157,10 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERYD_TASK_TIME_LIMIT = 5 * 60
 CELERYD_TASK_SOFT_TIME_LIMIT = 60
+
+# django REST framework
+REST_FRAMEWORK = {
+    'DEFAULT_RENDER_CLASSES': (
+        'rest_framework.renders.JSONRenderer',
+    )
+}
--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -6,5 +6,5 @@ from cride.circles.views import list_circles, create_circle
 
 urlpatterns = [
     path('circles/', list_circles),
-    path('circles/create', create_circle),
+    path('circles/create/', create_circle),
 ]
